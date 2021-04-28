7530080177197567f5a665aaeb092aab4f0f5d82
kenny <kenny.kuo.fs@gmail.com>
2021-04-28 23:00:46 +0800

    some try

--- ZeroJudge/zja164.cpp
+++ ZeroJudge/zja164.cpp
@@ -1,87 +1,81 @@
 #include <iostream>
 #include <tuple>
 #include <utility>
-#define Hirasawa_Yui_My_Wife ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-
-
-#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
-#pragma GCC target("fma,sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
-#pragma comment(linker, "/stack:200000000")
-
-
 using namespace std;
 int n,q;
 const int N=200005;
-int data[N],sum[4*N],front[4*N],back[4*N];
-typedef tuple<int,int,int,int> T;
-//best,sum,front,back
-//or
-//bestl,bestr,frontr,backl
-typedef pair<T,T> ptt;
+int data[N],sum[4*N],front[4*N],back[4*N],findex[4*N],bindex[4*N],mid[4*N],lindex[4*N],rindex[4*N];
 void build(int cur,int l,int r){
+	cout<<l<<' '<<r<<'\n';
 	if(l>=r)return;
-	if(l+1==r){sum[cur]=front[cur]=back[cur]=data[l];return;}
-	int m=l+r>>1;
+	if(l+1==r){
+		sum[cur]=front[cur]=back[cur]=mid[cur]=data[l];
+		findex[cur]=bindex[cur]=lindex[cur]=rindex[cur]=l;
+		return;
+	}
+	int m=(l+r)>>1;
+	cout<<"flag2\n";
 	build(cur<<1  ,l,m);
 	build(cur<<1|1,m,r);
+	
 	sum[cur]=sum[cur<<1]+sum[cur<<1|1];
-	front[cur]=max(front[cur<<1],sum[cur<<1]+front[cur<<1|1]);
-	back[cur]=max(back[cur<<1|1],sum[cur<<1|1]+back[cur<<1]);
+	mid[cur]=max(back[cur<<1]+front[cur<<1|1],max(mid[cur<<1],mid[cur<<1|1]));
+	if(mid[cur]==mid[cur<<1|1])
+		lindex[cur]=lindex[cur<<1|1],
+		rindex[cur]=rindex[cur<<1|1];
+	if(mid[cur]==back[cur<<1]+front[cur<<1|1])
+		lindex[cur]=bindex[cur<<1],
+		rindex[cur]=findex[cur<<1|1];
+	if(mid[cur]==mid[cur<<1])
+		lindex[cur]=lindex[cur<<1],
+		rindex[cur]=rindex[cur<<1];
+	
+	front[cur]=front[cur<<1];
+	findex[cur]=findex[cur<<1];
+	if(sum[cur<<1]+front[cur<<1|1]>front[cur])front[cur]=sum[cur<<1]+front[cur<<1|1],findex[cur]=findex[cur<<1|1];
+	
+	back[cur]=back[cur<<1|1];
+	bindex[cur]=bindex[cur<<1|1];
+	if(sum[cur<<1|1]+back[cur<<1]>back[cur])back[cur]=sum[cur<<1|1]+back[cur<<1],bindex[cur]=bindex[cur<<1];
+	cout<<"leave"<<l<<' '<<r<<'\n';return;
 }
-#define INF 80000000
-ptt query(int cur,int l,int r,int ql,int qr){
-	if(l>=r||r<=ql||l>=qr)return {make_tuple(-INF,-INF,-INF,-INF),make_tuple(-8787,-8787,-8787,-8787)};
-	if(l+1==r)return {make_tuple(data[l],data[l],data[l],data[l]),make_tuple(l,l,l,l)};
-	int m=l+r>>1;
-	int ans,s,f,b;
-	ptt pttl=query(cur<<1,l,m,ql,qr),pttr=query(cur<<1|1,m,r,ql,qr);
-	if(get<0>(pttl.first)==-INF)//left is out of range
-		return pttr;
-	if(get<0>(pttr.first)==-INF)
-		return pttl;
-	T tl=pttl.first,tr=pttr.first,ttl=pttl.second,ttr=pttr.second;
-	
-	T ansp=make_tuple(-INF,-INF,-INF,-INF);
-	
-	ans=max(get<0>(tl),max(get<0>(tr),get<3>(tl)+get<2>(tr)));
-	if(ans==get<0>(tr))
-		get<0>(ansp)=get<0>(ttr),
-		get<1>(ansp)=get<1>(ttr);
-	if(ans==get<3>(tl)+get<2>(tr))
-		get<0>(ansp)=get<3>(ttl),
-		get<1>(ansp)=get<2>(ttr);
-	if(ans==get<0>(tl))
-		get<0>(ansp)=get<0>(ttl),
-		get<1>(ansp)=get<1>(ttl);
-
-	s=get<1>(tl)+get<1>(tr);
-	
-	f=max(get<2>(tl),get<1>(tl)+get<2>(tr));
-	if(f==get<1>(tl)+get<2>(tr))
-		get<2>(ansp)=get<2>(ttr);
-	if(f==get<2>(tl))
-		get<2>(ansp)=get<2>(ttl);
-	
-	b=max(get<3>(tr),get<3>(tl)+get<1>(tr));
-	if(b==get<3>(tr))
-		get<3>(ansp)=get<3>(ttr);
-	if(b==get<3>(tl)+get<1>(tr))
-		get<3>(ansp)=get<3>(ttl);
-	return {make_tuple(ans,s,f,b),ansp};
+int f,fl;
+int ans,ansl,ansr;
+int toupdate;
+void query(int cur,int l,int r,int ql,int qr){
+	if(l>=r||r<=ql||qr<=l)return;
+	if(l>=ql&&r<=qr){
+		/*compare:
+			f+front[cur]
+			mid[cur]
+		  and then update ans
+		*/
+		toupdate=max(f+front[cur],mid[cur]);
+		//[fl,findex[cur]]   and   [lindex[cur],rindex[cur]]
+		if(toupdate>ans){
+			if(toupdate==mid[cur])
+				ansl=lindex[cur],ansr=rindex[cur];
+			if(toupdate==f+front[cur])
+				ansl=fl,ansr=findex[cur];
+		}
+		
+		//update f, fl
+		if(back[cur]>f+sum[cur])f=back[cur],fl=bindex[cur];
+		else if(f+sum[cur]>=0)f=f+sum[cur];//if f+sum[cur]==0, don't update because ans(l,r) l,r should be the least
+		else f=0,fl=r;
+	}
+	int m=(l+r)>>1;
+	query(cur<<1  ,l,m,ql,qr);
+	query(cur<<1|1,m,r,ql,qr);
 }
 int main(){
-	Hirasawa_Yui_My_Wife
-	int l,r,ans,i=1;
+	int i=1;
 	while(cin>>n>>q){
 		cout<<"Case "<<i++<<":\n";
 		for(int i=1;i<=n;++i)cin>>data[i];
 		build(1,1,n+1);
-		while(q--){
-			cin>>l>>r;
-			ptt tmp=query(1,1,n+1,l,r+1);
-			tie(l,r,ignore,ignore)=tmp.second;
-			ans=get<0>(tmp.first);
-			cout<<l<<' '<<r<<' '<<ans<<'\n';
-		}
+		cout<<"flag1"<<endl;
+		query(1,1,n+1,1,3);
+		cout<<ansl<<' '<<ansr<<' '<<ans<<'\n';
 	}
 }
