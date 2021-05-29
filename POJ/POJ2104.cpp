3e76549d45d937d7f359303a4cffb7804035886e
kenny <kenny.kuo.fs@gmail.com>
2021-05-29 14:32:59 +0800

    POJ2104 AC\!之前小錯誤而已

--- POJ/POJ2104.cpp
+++ POJ/POJ2104.cpp
@@ -3,17 +3,7 @@
 #define Hirasawa_Yui_My_Wife ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 using namespace std;
 int n,q;
-int sorted[100010],tree[20][100010],toleft[20][100010];//toleft as BIT
-void BITaddone(int dep,int ind){
-	for(int i=ind;i<=n;i+=i&(-i))
-		++toleft[dep][i];
-}
-int BITquery(int dep,int ind){
-	int ans=0;
-	for(int i=ind;i;i-=i&(-i))
-		ans+=toleft[dep][i];
-	return ans;
-}
+int sorted[100010],tree[20][100010],toleft[20][100010];
 void build(int l,int r,int dep){//build tree[dep] with tree[dep-1]
 	//[l,r)
 	if(l+1==r)return;
@@ -41,9 +31,10 @@ void build(int l,int r,int dep){//build tree[dep] with tree[dep-1]
 		}
 		if(left)
 			tree[dep][lind++]=tree[dep-1][i],
-			BITaddone(dep-1,i);//add toleft[dep][i]
+			toleft[dep-1][i]=toleft[dep-1][i-1]+1;//add toleft[dep][i]
 		else
-			tree[dep][rind++]=tree[dep-1][i];
+			tree[dep][rind++]=tree[dep-1][i],
+			toleft[dep-1][i]=toleft[dep-1][i-1];
 	}
 	build(l,m,dep+1);
 	build(m,r,dep+1);
@@ -51,16 +42,16 @@ void build(int l,int r,int dep){//build tree[dep] with tree[dep-1]
 int query(int l,int r,int ql,int qr,int dep,int k){
 	if(l+1==r)return sorted[l];
 	int m=(l+r)>>1;
-	int lcnt=BITquery(dep,qr-1)-BITquery(dep,ql-1);//num of numbers in [ql,qr) that go to the left
+	int lcnt=toleft[dep][qr-1]-toleft[dep][ql-1];//num of numbers in [ql,qr) that go to the left
 	//int rcnt=qr-ql-lcnt;
 	if(k<=lcnt){
-		int newl=l+BITquery(dep,ql-1)-BITquery(dep,l-1);//we don't need the nums in [l,ql) that go to the left
+		int newl=l+toleft[dep][ql-1]-toleft[dep][l-1];//we don't need the nums in [l,ql) that go to the left
 		int	newr=newl+lcnt;
 		return query(l,m,newl,newr,dep+1,k);
 	}else{
-		int newr=qr+BITquery(dep,r-1)-BITquery(dep,qr-1);//we don't need the nums in [qr,r) that go to the "right"
-		//r-((r-qr)-(BITquery(dep,r-1)-BITquery(dep,qr-1)))  =  qr+BITquery(dep,r-1)-BITquery(dep,qr-1)
-		int newl=newr-(ql-qr-lcnt);
+		int newr=qr+toleft[dep][r-1]-toleft[dep][qr-1];//we don't need the nums in [qr,r) that go to the "right"
+		//r-((r-qr)-(toleft[dep][r-1]-toleft[dep][qr-1]))  =  qr+toleft[dep][r-1]-toleft[dep][qr-1]
+		int newl=newr-(qr-ql-lcnt);
 		return query(m,r,newl,newr,dep+1,k-lcnt);
 	}
 }
