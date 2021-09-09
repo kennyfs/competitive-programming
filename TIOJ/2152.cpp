2a0e1ac879ddcdf4614ed123f6ba0faba55be627
kenny <kenny.kuo.fs@gmail.com>
2021-09-09 15:15:49 +0800

    tioj 2152 incomplete

--- TIOJ/2152.cpp
+++ TIOJ/2152.cpp
@@ -32,17 +32,18 @@ int main(){
             a[p--]=ind[opcnt++]=tmp[i];
         }
     }
+    cout<<"init p="<<p<<'\n';
     for(int i=1;i<=n;i+=k){//do b[i,min(n,i+k)], require things must be in a[1,j]
         int j=min(n,i+k);
         //take [1,j] and put [1,i-1] then [i,j]
         for(int l=1;l<=j;++l)op[opcnt++]='T',tmp[i]=a[i];
         fill(app,app+N,N);//O(n^2) is fine
-        for(int i=i;i<=j;++i)app[b[i]]=min(app[b[i]],i);
-        sort(tmp+1,tmp+n+1,[&](int i,int j){
+        for(int index=i;index<=j;++index)app[b[index]]=min(app[b[index]],index);
+        sort(tmp+1,tmp+j+1,[&](int i,int j){
             return app[i]<app[j];
         });
         tmp[j+1]=N;
-        p=j
+        p=j;
         for(int l=j;l;--l){
             if(tmp[l]==tmp[l+1]){
                 op[opcnt]='P';
@@ -55,10 +56,27 @@ int main(){
                 a[p--]=ind[opcnt++]=tmp[l];
             }
         }
+        cout<<"p="<<p<<'\n';
         //required b[i,j] are in the front
-
-        for(int l=1;l<=i;++l)op[opcnt]='P',ind[opcnt++]=a[l];
-
+        cout<<"tmp:";
+        for(int in=1;in<=j;++in)cout<<tmp[in]<<' ';
+        cout<<'\n';
+        for(int in=1;in<=j;++in)cout<<a[in]<<' ';
+        cout<<'\n';
+        for(int index=i,req,cur,h;index<=j;++index){
+			req=b[index];
+			h=1;
+			do{
+				cur=a[h++];
+				op[opcnt++]='T';
+			}while(cur==req);
+			--h;
+			op[opcnt++]='U';
+			for(;h;--h){
+				op[opcnt]='P';
+				ind[opcnt++]=a[h];
+			}
+		}
     }
     cout<<((opcnt-n)>>1)<<'\n';
     for(int i=0;i<opcnt;++i){
