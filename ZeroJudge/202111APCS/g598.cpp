132d965cb0fc504524254ecc4e278873af6281c3
kenny <kenny.kuo.fs@gmail.com>
2021-11-08 08:11:19 +0800

    zj g598 二分搜AC

--- ZeroJudge/202111APCS/g598.cpp
+++ ZeroJudge/202111APCS/g598.cpp
@@ -1,49 +1,61 @@
 #include <iostream>
 #include <vector>
-#include <utility>
+#include <cstring>
 #define Hirasawa_Yui_My_Wife ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define pii pair<int,int>
-#define F first
-#define S second
 #define pb push_back
+#define mid ((l+r)>>1)
 using namespace std;
-const int N=2e4+10;
+const int N=2e4+10,P=1e4+10;
 int n,m,p,k,color[N];
-vector<int> G[N];
 int oc(int c){return (c%2)?c+1:c-1;}
-void dfs(int u,int c){
+struct edge{int a,b;};
+vector<edge> E[N];
+vector<int> G[N],wrong;
+bool ok(int u,int c){
 	color[u]=c;
-	for(int i:G[u])if(!color[i])dfs(i,oc(c));
+	for(int i:G[u]){
+		if(color[i]==color[u])return 0;
+		if(!color[i]&&!ok(i,oc(c)))return 0;
+	}
+	return 1;
 }
-vector<pii> eq;
+bool check(int p){//return if wrong
+	memset(color,0,sizeof(color));
+	for(int i=0;i<n;++i)G[i].clear();
+	for(int i=0;i<=p;++i){
+		for(auto [a,b]:E[i]){
+			G[a].pb(b);
+			G[b].pb(a);
+		}
+	}
+	for(int i=0;i<n;++i)if(!color[i]&&!ok(i,1))return 1;
+	return 0;
+}
+void BS(){
+	int l=1,r=p;
+	if(!check(r))return;
+	while(l!=r){
+		if(check(mid))r=mid;
+		else l=mid+1;
+	}
+	E[l].clear();
+	wrong.pb(l);
+}
 int main(){
 	Hirasawa_Yui_My_Wife
 	cin>>n>>m;
+	int u,v;
 	while(m--){
-		int u,v;
 		cin>>u>>v;
-		G[u].pb(v);
-		G[v].pb(u);
-	}
-	int now=1;
-	for(int i=0;i<n;++i){
-		if(!color[i])dfs(i,now),now+=2;
+		E[0].pb({v,u});
 	}
 	cin>>p>>k;
-	int a,b;
-	vector<int> wrong;
-	for(int o=1;o<=p;++o){
-		eq.clear();
-		bool flag=0;
+	for(int i=1;i<=p;++i){
 		for(int j=0;j<k;++j){
-			cin>>a>>b;
-			if(flag)continue;
-			if(color[a]==color[b]){wrong.pb(o);flag=1;continue;}
-			for(auto [u,v]:eq)if((color[a]==u&&color[b]==v)||(color[a]==v&&color[b]==u)){wrong.pb(o);flag=1;break;}
-			eq.pb({color[a],oc(color[b])});
-			eq.pb({color[b],oc(color[a])});
+			cin>>u>>v;
+			E[i].pb({u,v});
 		}
 	}
+	for(int i=0;i<3;++i)BS();
 	for(int i:wrong)cout<<i<<'\n';
 }
-//可能是假解的賽中解
---
+++ ZeroJudge/202111APCS/g598FST.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#define Hirasawa_Yui_My_Wife ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define pii pair<int,int>
+#define F first
+#define S second
+#define pb push_back
+using namespace std;
+const int N=2e4+10;
+int n,m,p,k,color[N];
+vector<int> G[N];
+int oc(int c){return (c%2)?c+1:c-1;}
+void dfs(int u,int c){
+	color[u]=c;
+	for(int i:G[u])if(!color[i])dfs(i,oc(c));
+}
+vector<pii> eq;
+int main(){
+	Hirasawa_Yui_My_Wife
+	cin>>n>>m;
+	while(m--){
+		int u,v;
+		cin>>u>>v;
+		G[u].pb(v);
+		G[v].pb(u);
+	}
+	int now=1;
+	for(int i=0;i<n;++i){
+		if(!color[i])dfs(i,now),now+=2;
+	}
+	cin>>p>>k;
+	int a,b;
+	vector<int> wrong;
+	for(int o=1;o<=p;++o){
+		eq.clear();
+		bool flag=0;
+		for(int j=0;j<k;++j){
+			cin>>a>>b;
+			if(flag)continue;
+			if(color[a]==color[b]){wrong.pb(o);flag=1;continue;}
+			for(auto [u,v]:eq)if((color[a]==u&&color[b]==v)||(color[a]==v&&color[b]==u)){wrong.pb(o);flag=1;break;}
+			eq.pb({color[a],oc(color[b])});
+			eq.pb({color[b],oc(color[a])});
+		}
+	}
+	for(int i:wrong)cout<<i<<'\n';
+}
+//假解的賽中解
