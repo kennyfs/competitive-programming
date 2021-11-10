f22a237c86aeec7282d513a7d0e1ac599d915976
kenny <kenny.kuo.fs@gmail.com>
2021-11-10 18:13:19 +0800

    cses 1678

--- CSES/1678.cpp
+++ CSES/1678.cpp
@@ -11,36 +11,26 @@ const ll INF=8e7*wiwiho;
 const int N=1e5+10;
 int n,m,pre[N];
 vector<int> G[N],ans;
-bool vis[N];
-stack<int> S;
-void output(int u){
-	int v=u;
-	stack<int> ans;
-	ans.push(u);
-	do{
-		ans.push(v=pre[v]);
-	}while(v!=u);
-	cout<<ans.size()<<'\n';
-	while(!ans.empty())cout<<ans.top()<<' ',ans.pop();
-}
+short vis[N];
+stack<int> S,T;
 bool dfs(int u){//if there's a cycle
 	vis[u]=1;
 	S.push(u);
 	for(int v:G[u]){
 		if(vis[v]==1){
-			cout<<"v="<<v<<endl;
 			ans.pb(v);
-			while(S.top()!=v)cout<<S.top()<<endl,ans.pb(S.top()),S.pop();
+			while(S.top()!=v)ans.pb(S.top()),S.pop();
 			ans.pb(v);
 			return 1;
 		}
-		if(!vis[v])if(dfs(v))return 1;
+		if(!vis[v]&&dfs(v))return 1;
 	}
 	S.pop();
 	vis[u]=2;
 	return 0;
 }
 int main(){
+	Hirasawa_Yui_My_Wife
 	cin>>n>>m;
 	while(m--){
 		int u,v;
@@ -48,12 +38,13 @@ int main(){
 		G[u].pb(v);
 	}
 	int u;
-	for(int i=1;i<=n;++i)
+	for(int i=1;i<=n;++i){
 		if(!vis[i]&&(u=dfs(i))){
 			reverse(ans.begin(),ans.end());
 			cout<<ans.size()<<'\n';
 			for(int i:ans)cout<<i<<' ';
 			return 0;
 		}
+	}
 	cout<<"IMPOSSIBLE";
 }
