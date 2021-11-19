e39c523d3162c10fb13f14d86b2a513151f01945
kenny <kenny.kuo.fs@gmail.com>
2021-11-19 22:18:13 +0800

    TIOJ 1429 edit

--- TIOJ/1429.cpp
+++ TIOJ/1429.cpp
@@ -4,13 +4,12 @@
 #define Hirasawa_Yui_My_Wife ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define pb emplace_back
 #define ll long long
-#define pql priority_queue<ll,vector<ll>,cmp>
+#define pql priority_queue<ll>
 using namespace std;
 const int N=1e5+10;
 int n,m,c[N],l[N];
 ll ans;
 vector<int> G[N],root;
-struct cmp{bool operator()(ll a,ll b){return a<b;}};
 void join(pql& a,pql& b){
 	if(a.size()<b.size())swap(a,b);
 	while(!b.empty())a.push(b.top()),b.pop();
@@ -22,7 +21,6 @@ ll dfs(int u,int fa,pql& pq){
 	if(G[u].size()==0)goto done;
 	for(int v:G[u]){
 		if(v==fa)continue;
-		while(!tmp.empty())tmp.pop();
 		sum+=dfs(v,u,tmp);
 		join(pq,tmp);
 	}
@@ -39,9 +37,7 @@ int main(){
 		if(b==0)root.pb(i);
 		else G[b].pb(i);
 	}
-	for(int i:root){
-		pql a;
-		dfs(i,i,a);
-	}
+	pql a;
+	dfs(1,1,a);
 	cout<<ans;
 }
