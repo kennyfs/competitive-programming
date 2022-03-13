571995e75fdcf7fd48e8042a64e8a46f7090bf01
kenny <kenny.kuo.fs@gmail.com>
2022-03-13 12:33:27 +0800

    pR AC，雖然是抄扣

--- vir TOI初選/pR.cpp
+++ vir TOI初選/pR.cpp
@@ -1,37 +1,42 @@
 #include <iostream>
 #include <vector>
+#define Hirasawa_Yui_My_Wife ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define wiwiho 25
 #define pb push_back
-int const N=1e5+10;
+int const N=1e5+10,inf=8e7*wiwiho;
 using namespace std;
 struct p{
 	int v,w;
 };
+struct fs{
+	int f,s;
+	fs():f(0),s(0){}
+	void operator()(int x){
+		if(x==f)return;
+		if(x>f)swap(f,x);
+		if(x>s)swap(s,x);
+	}
+}ans;
 vector<p> G[N];
 int n,total_lg,total_seclg,lg[N],seclg[N];
 bool vis[N];
-void DP(int u){
-	cout<<"in:"<<u<<'\n';
+fs DP(int u){
 	vis[u]=1;
-	int tmplg=0,tmpseclg=0;
+	fs dpu;
 	for(auto [v,w]:G[u]){
 		if(vis[v])continue;
-		DP(v);
-		tmplg=max(tmplg,lg[u]+lg[v]+w);
-		int toup[3]={lg[u]+seclg[v]+w,seclg[u]+lg[v]+w,lg[u]};
-		for(int i:toup){cout<<i<<',';if(i<tmplg)tmpseclg=max(tmpseclg,i);}
-		cout<<'\n';
-		cout<<tmplg<<' '<<tmpseclg<<'\n';
-		lg[u]=max(lg[u],lg[v]+w);
-		if(lg[u]!=lg[v]+w){
-			seclg[u]=max(seclg[u],lg[v]+w);
-		}
-		seclg[u]=max(seclg[u],seclg[v]+w);
+		fs dpv=DP(v);
+		ans(dpu.f+dpv.f+w);
+		ans(dpu.f+dpv.s+w);
+		ans(dpu.s+dpv.f+w);
+		
+		dpu(dpv.f+w);
+		dpu(dpv.s+w);
 	}
-	total_lg=max(total_lg,tmplg);
-	total_seclg=max(total_seclg,tmpseclg);
-	cout<<"out:"<<u<<'\n';
+	return dpu;
 }
 int main(){
+	Hirasawa_Yui_My_Wife
 	cin>>n;
 	for(int i=0,u,v,w;i<n-1;++i){
 		cin>>u>>v>>w;
@@ -39,5 +44,5 @@ int main(){
 		G[v].pb({u,w});
 	}
 	DP(0);
-	cout<<total_lg<<' '<<total_seclg<<'\n';
+	cout<<ans.s<<'\n';
 }
